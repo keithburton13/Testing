8ffbb3f69ec4e9da3a6738cf2fb8c08999cb79f1
Keith Burton <keith_burton@live.co.uk>
2015-09-20 00:25:32 +0100

    Initial Commit

---
+++ Testing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testing
+//
+//  Created by Keith Burton on 20/09/2015.
+//  Copyright © 2015 Keith Burton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
